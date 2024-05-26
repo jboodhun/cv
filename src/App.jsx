a52e532dc361f2c009b52bdd50ba9e13557c6f3c
Jay Boodhun <jay.bdhun@gmail.com>
2024-05-26 08:33:41 +0100

    feat: Add DaisyUI css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Logo from './components/Logo';
+import TestComponent from './components/TestComponent';
 
 const App = () => {
   return (
     <>
       <Logo />
       <p>Hello World!</p>
+      <TestComponent />
     </>
   );
 };
