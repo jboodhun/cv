8f21df05cca1aba6b29e993a731d675b9f4f9b09
Jay Boodhun <jay.bdhun@gmail.com>
2024-05-27 09:20:32 +0100

    fix: remove vite sentry

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
