ef69d632f1d48c66d44c739c8fb458d4f141787d
Jay Boodhun <jay.bdhun@gmail.com>
2024-05-27 13:06:23 +0100

    feat: add icons to switch theme.

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -1,12 +1,17 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from '../context/ThemeContext';
+import { Light, Dark } from './icons';
 
 const ThemeSwitcher = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
 
   return (
-    <button type="button" onClick={toggleTheme}>
-      {theme === 'light' ? 'Switch to Dark Mode' : 'Switch to Light Mode'}
+    <button
+      type="button"
+      onClick={toggleTheme}
+      className={`theme-switcher ${theme === 'light' ? 'dark' : 'light'}`}
+    >
+      {theme === 'light' ? <Dark /> : <Light />}
     </button>
   );
 };
